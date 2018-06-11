4babe7a8092bb10827b6380d8149e9f7872b78ec
Miodrag Seslija <mseslija@itekako.com>
2018-06-11 15:31:00 +0200

    python

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-with open('hrtool_api/__init__.py', 'r') as fd:
+with open('mysite/__init__.py', 'r') as fd:
     version_re = r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]'
     version = re.search(version_re, fd.read(), re.MULTILINE).group(1)
 
